1a9bf297e10924a885e3f03bf9616be4d7096403
zerotohero200222 <aebharath17@gmail.com>
2025-07-07 17:07:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,30 +1,36 @@
 variable "env" {
   type        = string
-  description = "Environment name (e.g., dev, test, prod)."
+  description = "Environment name (dev, test, prod)"
 }
 
 variable "resource_group_location" {
   type        = string
-  description = "Location for all resources."
+  description = "Azure region where resources are created"
 }
 
 variable "resource_group_name_prefix" {
   type        = string
-  description = "Prefix for the resource group name."
+  description = "Prefix used for the resource group"
 }
 
 variable "dns_zone_name" {
   type        = string
   default     = null
-  description = "Optional DNS zone name."
+  description = "DNS zone name, optional"
 }
 
 variable "dns_ttl" {
   type        = number
-  description = "Time To Live (TTL) for the DNS record."
+  description = "Time to live for DNS A record"
 }
 
 variable "dns_records" {
   type        = list(string)
-  description = "List of IP addresses for the A record."
+  description = "IP addresses for A record"
 }
+
+# Backend variables for remote state
+variable "backend_resource_group" {}
+variable "backend_storage_account" {}
+variable "backend_container_name" {}
+variable "backend_key" {}
