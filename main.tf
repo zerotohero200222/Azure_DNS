c01eed970c366cbccb6e24ef355839ac4dc97e33
zerotohero200222 <aebharath17@gmail.com>
2025-07-07 15:42:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
+  prefix = "${var.resource_group_name_prefix}-${var.env}"
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = random_pet.rg_name.id
   location = var.resource_group_location
+  tags = {
+    environment = var.env
+  }
 }
 
 resource "random_string" "azurerm_dns_zone_name" {
@@ -19,9 +22,12 @@ resource "azurerm_dns_zone" "zone" {
   name = (
     var.dns_zone_name != null ?
     var.dns_zone_name :
-    "www.${random_string.azurerm_dns_zone_name.result}.azurequickstart.org"
+    "www.${random_string.azurerm_dns_zone_name.result}.${var.env}.azurequickstart.org"
   )
   resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    environment = var.env
+  }
 }
 
 resource "azurerm_dns_a_record" "record" {
@@ -30,4 +36,7 @@ resource "azurerm_dns_a_record" "record" {
   zone_name           = azurerm_dns_zone.zone.name
   ttl                 = var.dns_ttl
   records             = var.dns_records
+  tags = {
+    environment = var.env
+  }
 }
